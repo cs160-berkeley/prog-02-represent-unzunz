2fb8808448a1c6f2e6fe488b6080ffcd0103447b
Unzi Park <upark.go@gmail.com>
2016-03-03 03:14:54 -0800

    Add accelerator sensor??

--- Represent/wear/src/main/java/com/cs160/unzi/represent/MainActivity.java
+++ Represent/wear/src/main/java/com/cs160/unzi/represent/MainActivity.java
@@ -1,9 +1,13 @@
 package com.cs160.unzi.represent;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 import android.support.wearable.view.BoxInsetLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,9 +19,20 @@ import java.util.Locale;
 
 public class MainActivity extends WearableActivity {
 
+    private SensorManager mSensorManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+            Log.i("ACCELEROMETER", "IS WORKINNN");
+        } else {
+            // booo no accelerometer
+        }
+
+
+
     }
 }
