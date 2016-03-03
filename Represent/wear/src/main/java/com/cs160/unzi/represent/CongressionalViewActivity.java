ed93a0daf5e3b679e9e7a04e1b9b3e3bac321c88
Unzi Park <upark.go@gmail.com>
2016-03-03 01:30:11 -0800

    It works!!!!!!

--- Represent/wear/src/main/java/com/cs160/unzi/represent/CongressionalViewActivity.java
+++ Represent/wear/src/main/java/com/cs160/unzi/represent/CongressionalViewActivity.java
@@ -1,9 +1,16 @@
 package com.cs160.unzi.represent;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.wearable.view.CardFragment;
+import android.support.wearable.view.DotsPageIndicator;
+import android.support.wearable.view.FragmentGridPagerAdapter;
+import android.support.wearable.view.GridViewPager;
 import android.support.wearable.view.WatchViewStub;
 import android.util.Log;
 import android.view.View;
@@ -12,7 +19,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class CongressionalViewActivity extends Activity {
+public class CongressionalViewActivity extends FragmentActivity {
 
     private TextView mTextView;
 
@@ -24,23 +31,65 @@ public class CongressionalViewActivity extends Activity {
         Intent intent = getIntent();
         ArrayList<String> extras = intent.getStringArrayListExtra("REGION_REPS");
 
-        LinearLayout hello = (LinearLayout) findViewById(R.id.watch_congressional);
+//        LinearLayout hello = (LinearLayout) findViewById(R.id.watch_congressional);
 //        mTextView = (TextView) findViewById(R.id.reps_output);
 //        mTextView.setText("WHYS IS THIS NOT WORKIGN HELLO SLKDFJSLKDJF BYSDFLKSDF???? WHY IS DIS NOT NOT WORKI?SDFSDF?");
 //        Log.i("We in here", mTextView.getText().toString());
 
 //        Log.i("EXTRAS? ", Integer.toString(extras.size()));
 
-        if (extras != null) {
-            for (String member : extras) {
-//                Log.i("MEMBER: ", member);
-                TextView new_rep = new TextView(this);
-                new_rep.setText(member);
-                RepOnClickListener click_listener = new RepOnClickListener();
-                click_listener.setRepName(member);
-                new_rep.setOnClickListener(click_listener);
-                hello.addView(new_rep);
-            }
+        final DotsPageIndicator mPageIndicator;
+        final GridViewPager mViewPager;
+
+        final String[][] data = {
+            { "Row 0, Col 0", "Row 0, Col 1", "Row 0, Col 2" },
+            { "Row 1, Col 0", "Row 1, Col 1", "Row 1, Col 2" },
+            { "Row 2, Col 0", "Row 2, Col 1", "Row 2, Col 2" }
+        };
+
+        // Get UI references
+        mPageIndicator = (DotsPageIndicator) findViewById(R.id.page_indicator);
+        mViewPager = (GridViewPager) findViewById(R.id.pager);
+
+        // Assigns an adapter to provide the content for this pager
+        mViewPager.setAdapter(new GridPagerAdapter(getFragmentManager(), data));
+        mPageIndicator.setPager(mViewPager);
+
+
+//        if (extras != null) {
+//            for (String member : extras) {
+////                Log.i("MEMBER: ", member);
+//                TextView new_rep = new TextView(this);
+//                new_rep.setText(member);
+//                RepOnClickListener click_listener = new RepOnClickListener();
+//                click_listener.setRepName(member);
+//                new_rep.setOnClickListener(click_listener);
+//                hello.addView(new_rep);
+//            }
+//        }
+    }
+    private static final class GridPagerAdapter extends FragmentGridPagerAdapter {
+
+        String[][] mData;
+
+        private GridPagerAdapter(FragmentManager fm, String[][] data) {
+            super(fm);
+            mData = data;
+        }
+
+        @Override
+        public Fragment getFragment(int row, int column) {
+            return (CardFragment.create("CardFragment", mData[row][column]));
+        }
+
+        @Override
+        public int getRowCount() {
+            return mData.length;
+        }
+
+        @Override
+        public int getColumnCount(int row) {
+            return mData[row].length;
         }
     }
 
@@ -62,4 +111,7 @@ public class CongressionalViewActivity extends Activity {
         sendIntent.putExtra("SELECTED_REP", rep_name);
         startService(sendIntent);
     };
+
+
+
 }
