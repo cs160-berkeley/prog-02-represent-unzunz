e04fa905ea5a999a8d766d51c8ed96eeba151a8b
Unzi Park <upark.go@gmail.com>
2016-03-03 18:46:10 -0800

    idk

--- Represent/wear/src/main/java/com/cs160/unzi/represent/MainActivity.java
+++ Represent/wear/src/main/java/com/cs160/unzi/represent/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -25,18 +26,53 @@ public class MainActivity extends WearableActivity {
     private Sensor mSensor;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         if (mSensor != null) {
             Log.i("ACCELEROMETER", "IS WORKINNN");
-            Intent intent = new Intent(this, CongressionalViewActivity.class);
+            Intent intent = new Intent(getBaseContext(), CongressionalViewActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             Log.i("WATCHLISTENER", "92123");
             intent.putExtra("LOCATION", "92123");
             startActivity(intent);
         }
+//        Log.i("ACCELEROMETER", "IS WORKINNN");
+//        Intent intent = new Intent(getBaseContext(), CongressionalViewActivity.class);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        Log.i("WATCHLISTENER", "92123");
+//        intent.putExtra("LOCATION", "92123");
+//        startActivity(intent);
+        // Do something with this sensor value.
+
     }
+
+    private final SensorEventListener mSensorListener = new SensorEventListener() {
+
+        @Override
+        public final void onSensorChanged(SensorEvent event) {
+                // Do something with this sens
+                // or value.
+
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+        @Override
+        protected void onResume() {
+            super.onResume();
+            mSensorManager.registerListener(mSensorListener,  mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+    @Override
+        protected void onPause() {
+            mSensorManager.unregisterListener(mSensorListener);
+            super.onPause();
+        }
 }
